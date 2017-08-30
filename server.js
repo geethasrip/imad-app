9748ddfd256459229f4cde2498874fda01d38d97
geethasrip <25365076+geethasrip@users.noreply.github.com>
2017-08-30 17:31:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 var articles={
- ArticleOne:
+ 'article-One':
 {
     title:'Article one',
     heading:'Article-one',
